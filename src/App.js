f1c2c3e276ec0fa08914724f83df32e77c68b3ac
Carlos Vallejo <cvallejo.ec@gmail.com>
2021-01-13 15:56:26 -0500

    New routing

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import { HashRouter, Route } from 'react-router-dom';
+import { HashRouter, Route, Switch } from 'react-router-dom';
 
 import Customer from './pages/customer/Customer.jsx';
 import Owner from './pages/owner/Owner.jsx';
@@ -7,10 +7,12 @@ import Owner from './pages/owner/Owner.jsx';
 function App() {
   return (
     <div className="App">
-      <HashRouter basename="/">
-        <Route exact path="/" component={Customer} />
-        <Route exact path="/customer" component={Customer} />
-        <Route exact path="/owner" component={Owner} />
+      <HashRouter basename={process.env.PUBLIC_URL}>
+        <Switch>
+          <Route exact path="/" component={Customer} />
+          <Route exact path="/customer" component={Customer} />
+          <Route exact path="/owner" component={Owner} />
+        </Switch>
       </HashRouter>
     </div>
   );
